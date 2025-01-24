a51872021e823b15646af2567e8d13f70e30defb
jai1623 <sanjay23@meta.com>
2025-01-24 09:26:45 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "2378375702514825",
+          pixelId: "639089138642524",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
