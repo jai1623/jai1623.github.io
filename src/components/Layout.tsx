3f6f21994ec0597fbf9a39c6fc55aaafb43a41a2
jai1623 <sanjay23@meta.com>
2025-05-09 11:56:14 -0700

    Update Layo.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,22 +25,23 @@ export const Layout: React.FC = ({ children }) => {
   return (
     <FacebookPixelProvider>
 
-<script>{`
-    var __cbSdkHost = "https://sdk.leowu.uk/";
-    var __cbSdkSettings={'cbq':{destinations:[{name:"cloudbridge"},],
-    "host":__cbSdkHost,"globalVar":"cbq",}};
-    !function(a,h,e,v,n,t,s)
-      {if(a.cbq)return;n=a.cbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!a._cbq)a._cbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=h.createElement(e);t.async=!0;
-      t.src=v;s=h.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      "https://dhtj0romidohy.cloudfront.net/"+'sdk/capig-events.js');
-    cbq('init', "2378375702514825");
-    cbq('loadConfig', "2378375702514825");
-    cbq('track', 'PageView');`}
-    </script>
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '639089138642524');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=639089138642524&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
     
       <Helmet
         title="Altmeta.org Pixel Test"
